d338546f6c4d02d3ab4f4de457c4558a671bf92f
arclinesoft <arlincesoft@gmail.com>
2021-03-17 16:39:08 +0900

    add serve page webapp

--- cmd/frontend/main.go
+++ cmd/frontend/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "log"
+import (
+	"bytes"
+	"context"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
-	log.Print("Hello")
+	render := func(filename string, data interface{}) ([]byte, error) {
+		tmpl, err := template.ParseFiles(filename)
+		if err != nil {
+			return nil, err
+		}
+
+		var buf bytes.Buffer
+		err = tmpl.Execute(&buf, "index")
+
+		return buf.Bytes(), nil
+	}
+
+	servePage := func(ctx context.Context, w http.ResponseWriter, filename string, data interface{}) {
+		buf, err := render(filename, data)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		if _, err := io.Copy(w, bytes.NewBuffer(buf)); err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		servePage(ctx, w, "templates/index.html", "data")
+	})
+
+	srv := http.Server{
+		Addr:    ":3000",
+		Handler: mux,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 }
